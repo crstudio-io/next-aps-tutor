1623ce8da6f14733fba1b127f8e24f39b26f3b30
aquashdw <aquashdw@gmail.com>
2024-08-05 17:42:01 +0900

    styled <pre>s and copy to clipboard buttons (doesn't work yet)

--- src/app/problems/[id]/page.tsx
+++ src/app/problems/[id]/page.tsx
@@ -1,6 +1,7 @@
 import { Metadata } from "next";
 import Link from "next/link";
 import { notFound } from "next/navigation";
+import { OverlayTrigger, Tooltip } from "react-bootstrap";
 
 export const metadata: Metadata = {
   title: "Problem",
@@ -18,6 +19,7 @@ export default async function ProblemDetail({params}: { params: { id: string } }
   metadata.title = "Problem " + id;
   const problem = await getProblem(id);
   const examples = problem.examples;
+
   return (
     <main className="row justify-content-center">
       <div className="col-sm-11 col-md-10 col-lg-8">
@@ -29,23 +31,40 @@ export default async function ProblemDetail({params}: { params: { id: string } }
           <p>{problem.probDesc}</p>
         </section>
         <section className="mb-3">
-          <h2>입력</h2>
+          <h2 className="fs-3">입력</h2>
           <p>{problem.inputDesc}</p>
         </section>
         <section className="mb-3">
-          <h2>출력</h2>
+          <h2 className="fs-3">출력</h2>
           <p>{problem.outputDesc}</p>
         </section>
         <section className="mb-3">
           {examples.map((elem: { id: string, inputExample: string, outputExample: string }, index: number) =>
             <div key={elem.id} className="row mb-2">
               <div className="col-12 col-md-6">
-                <h3 className="mb-1">입력 예시 {index + 1}</h3>
-                <pre>{elem.inputExample}</pre>
+                <h3 className="fs-4">입력 예시 {index + 1}</h3>
+                <div className="position-relative">
+                  <div className="w-100 d-flex justify-content-end position-absolute top-0 p-1">
+                    <OverlayTrigger placement="top" overlay={(<Tooltip id="tooltip">Copy to clipboard</Tooltip>)}>
+                      <button className="border-0 bg-transparent">
+                        <i className="bi bi-clipboard"></i></button>
+                    </OverlayTrigger>
+                  </div>
+                  <pre className="bg-info-subtle rounded p-2 fs-6">{elem.inputExample}</pre>
+                </div>
               </div>
               <div className="col-12 col-md-6">
-                <h3 className="mb-1">출력 예시 {index + 1}</h3>
-                <pre>{elem.outputExample}</pre>
+                <h3 className="fs-4">출력 예시 {index + 1}</h3>
+                <div className="position-relative">
+                  <div className="w-100 d-flex justify-content-end position-absolute top-0 p-1">
+                    <OverlayTrigger placement="top" overlay={(<Tooltip id="tooltip">Copy to clipboard</Tooltip>)}>
+                      <button className="border-0 bg-transparent"
+                              data-bs-title="Copy to clipboard">
+                        <i className="bi bi-clipboard"></i></button>
+                    </OverlayTrigger>
+                  </div>
+                  <pre className="bg-info-subtle rounded p-2 fs-6">{elem.outputExample}</pre>
+                </div>
               </div>
             </div>)}
         </section>

6ebe4cb7a4a8f8f236880c888db438cd197c2bc9
aquashdw <aquashdw@gmail.com>
2024-08-06 12:59:58 +0900

    clipboard changes shapes!

--- src/components/problems/clipboard-btn.tsx
+++ src/components/problems/clipboard-btn.tsx
@@ -1,13 +1,30 @@
 "use client";
 
 import { OverlayTrigger, Tooltip } from "react-bootstrap";
+import { useState } from "react";
 
 export default function ClipboardButton({value}: { value: string }) {
-  const onClick = () => {
-    navigator.clipboard.writeText(value);
+  const initTooltip = "Copy to clipboard";
+  const initIcon = "bi bi-clipboard";
+  const [tooltip, setTooltip] = useState(initTooltip);
+  const [icon, setIcon] = useState(initIcon)
+
+  const reset = () => {
+    setTooltip(initTooltip);
+    setIcon(initIcon);
   }
-  return (<OverlayTrigger placement="top" overlay={(<Tooltip id="tooltip">Copy to clipboard</Tooltip>)}>
+
+  const onClick = async () => {
+    await navigator.clipboard.writeText(value);
+    setTooltip("Copied!");
+    setIcon("bi bi-clipboard-check");
+    setTimeout(reset, 3000);
+  }
+  return (<OverlayTrigger
+    placement="left"
+    overlay={(<Tooltip>{tooltip}</Tooltip>)}
+  >
     <button className="border-0 bg-transparent" onClick={onClick}>
-      <i className="bi bi-clipboard"></i></button>
+      <i className={icon}></i></button>
   </OverlayTrigger>)
 }

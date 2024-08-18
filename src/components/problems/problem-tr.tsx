ef6eb4d1836a6319bf865fab1c135fa62f1cf727
aquashdw <aquashdw@gmail.com>
2024-08-18 15:17:38 +0900

    table row to link

--- src/components/problems/problem-tr.tsx
+++ src/components/problems/problem-tr.tsx
@@ -1,17 +1,16 @@
-import Link from "next/link";
+"use client";
+import {useRouter} from "next/navigation";
 
-export default function ProblemTr({id, title,}
-                                    : { id: number, title: string, }) {
-  return (<tr>
-    <td>
-      <Link href={`/problems/${id}`}>
-        {id}
-      </Link>
-    </td>
-    <td>
-      <Link href={`/problems/${id}`}>
-        {title}
-      </Link>
-    </td>
+export default function ProblemTr({id, title,}: {
+  id: number,
+  title: string,
+}) {
+  const router = useRouter();
+  const onClick = () => {
+    router.push(`/problems/${id}`)
+  }
+  return (<tr className="clickable" onClick={onClick}>
+    <td>{id}</td>
+    <td>{title}</td>
   </tr>);
 }

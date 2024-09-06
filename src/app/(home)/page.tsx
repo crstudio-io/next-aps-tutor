6675c2f328f57d7f2284acb7e8e0ec42011c6dc8
aquashdw <aquashdw@gmail.com>
2024-09-06 17:27:21 +0900

    edit landing page

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Link from "next/link";
 
 export const metadata: Metadata = {
   title: "Home",
@@ -6,8 +7,13 @@ export const metadata: Metadata = {
 
 export default function Home() {
   return (
-    <main>
-      <h1>Hello World!</h1>
+    <main className="d-flex flex-column justify-content-center align-items-center">
+      <h1>CRStudio APS Tutor</h1>
+      <p>
+        This site is a toy project by <Link href="https://aquashdw.github.io" target="_blank">aquashdw</Link>,
+        to create algorithm problem solving practice service like Leetcode, Baekjoon Online Judge, Hackerrank and more.
+      </p>
+      <p>It is not intended to be an actual service, but feel free to have a look around.</p>
     </main>
   );
 }

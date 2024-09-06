1d0026e824ca774235dd8e1f2d8b13f62c90ee7c
aquashdw <aquashdw@gmail.com>
2024-09-06 16:43:01 +0900

    /profile to server component

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,7 +1,14 @@
-"use client";
+import type { Metadata } from "next";
+import { getSession } from "@/lib/session";
+import { redirect } from "next/navigation";
 
+export const metadata: Metadata = {
+  title: "Profile",
+};
 // using client to fetch user info here in the future
-export default function Profile() {
+export default async function Profile() {
+  const session = await getSession();
+  if (!session.signedIn) redirect("/signin");
   return <main>
     <h1>Nothing here...yet.</h1>
   </main>;

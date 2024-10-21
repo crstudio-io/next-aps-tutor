47fccf4609a9a79efb0cdc9f07dfc238cf4fa46d
aquashdw <aquashdw@gmail.com>
2024-10-21 13:36:44 +0900

    swap message for code signup

--- src/app/signup/verify/page.tsx
+++ src/app/signup/verify/page.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { verifySignUp } from "@/app/signup/verify/actions";
+import Link from "next/link";
 
 export const metadata: Metadata = {
   title: "Sign Up",
@@ -8,7 +9,20 @@ export const metadata: Metadata = {
 export default async function ProcessSignUp({searchParams}: { searchParams: { [key: string]: string | undefined } }) {
   await verifySignUp(searchParams.token ?? "");
   return <main>
+    {searchParams.code !== undefined ? <CodeMessage/> : <RequestMessage/>}
+  </main>;
+}
+
+function CodeMessage() {
+  return <>
+    <h1 className="mb-3">Your account is ready!</h1>
+    <p><Link href={`/signin`}>Try signing in</Link></p>
+  </>
+}
+
+function RequestMessage() {
+  return <>
     <h1 className="mb-3">Your request is accepted.</h1>
     <p>Please wait until we check and accept your request.</p>
-  </main>;
+  </>
 }
